b6e9d85fe3abea941868d5b471b38a84df9c4273
wuyilong <791128058@qq.com>
2024-07-22 15:50:17 +0800

    打包

--- hj-demo-app/src/main/java/com/hj/demo/app/HjDemoApplication.java
+++ hj-demo-app/src/main/java/com/hj/demo/app/HjDemoApplication.java
@@ -1,32 +1,14 @@
 
 package com.hj.demo.app;
 
-import io.nop.boot.NopApplication;
-import io.nop.core.initialize.CoreInitialization;
-import io.nop.quarkus.core.QuarkusIntegration;
-import io.quarkus.runtime.Quarkus;
-import io.quarkus.runtime.ShutdownEvent;
-import io.quarkus.runtime.StartupEvent;
-import io.quarkus.runtime.annotations.QuarkusMain;
+import org.noear.solon.Solon;
+import org.noear.solon.annotation.SolonMain;
 
-import jakarta.enterprise.event.Observes;
-
-@QuarkusMain
+@SolonMain
 public class HjDemoApplication {
-    static String[] globalArgs;
-
-    public void start(@Observes StartupEvent event) {
-        QuarkusIntegration.start();
-
-        new NopApplication().run(globalArgs);
-    }
-
-    public void stop(@Observes ShutdownEvent event) {
-        CoreInitialization.destroy();
-    }
 
     public static void main(String... args) {
-        globalArgs = args;
-        Quarkus.run(args);
+        Solon.start(HjDemoApplication.class, args);
     }
+
 }
