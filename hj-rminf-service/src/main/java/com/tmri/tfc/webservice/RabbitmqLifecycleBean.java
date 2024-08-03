7c3ccf7ce12f1ef4757af94638734206eb521b1e
wuyilong <791128058@qq.com>
2024-08-03 08:15:30 +0800

    打包

--- hj-rminf-service/src/main/java/com/tmri/tfc/webservice/RabbitmqLifecycleBean.java
+++ hj-rminf-service/src/main/java/com/tmri/tfc/webservice/RabbitmqLifecycleBean.java
@@ -8,6 +8,7 @@ import org.noear.solon.Solon;
 import org.noear.solon.Utils;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.core.bean.LifecycleBean;
+import org.noear.solon.core.runtime.NativeDetector;
 
 import java.nio.charset.StandardCharsets;
 
@@ -25,38 +26,39 @@ public class RabbitmqLifecycleBean implements LifecycleBean {
 
     @Override
     public void start() throws Throwable {
-        String host = Solon.cfg().get("solon.cloud.rabbitmq.server").split(":")[0];
-        int port = Integer.parseInt(Solon.cfg().get("solon.cloud.rabbitmq.server").split(":")[1]);
+        if (!NativeDetector.isNotAotRuntime()) {
+            String host = Solon.cfg().get("solon.cloud.rabbitmq.server").split(":")[0];
+            int port = Integer.parseInt(Solon.cfg().get("solon.cloud.rabbitmq.server").split(":")[1]);
 
-        connectionFactory = new ConnectionFactory();
+            connectionFactory = new ConnectionFactory();
 
-        // 配置连接信息
-        connectionFactory.setHost(host);
-        connectionFactory.setPort(port);
-        connectionFactory.setRequestedHeartbeat(30);
+            // 配置连接信息
+            connectionFactory.setHost(host);
+            connectionFactory.setPort(port);
+            connectionFactory.setRequestedHeartbeat(30);
 
-        String username = Solon.cfg().get("solon.cloud.rabbitmq.username");
-        if (Utils.isNotEmpty(username)) {
-            connectionFactory.setUsername(username);
+            String username = Solon.cfg().get("solon.cloud.rabbitmq.username");
+            if (Utils.isNotEmpty(username)) {
+                connectionFactory.setUsername(username);
+            }
+            String password = Solon.cfg().get("solon.cloud.rabbitmq.password");
+            if (Utils.isNotEmpty(password)) {
+                connectionFactory.setPassword(password);
+            }
+
+            // 网络异常自动连接恢复
+            connectionFactory.setAutomaticRecoveryEnabled(true);
+            // 每5秒尝试重试连接一次
+            connectionFactory.setNetworkRecoveryInterval(5000L);
+
+            //创建连接
+            connection = connectionFactory.newConnection();
+
+            channel = connection.createChannel();
+            // 启用消息确认
+            channel.confirmSelect();
+            eventPropsDefault = newEventProps().build();
         }
-        String password = Solon.cfg().get("solon.cloud.rabbitmq.password");
-        if (Utils.isNotEmpty(password)) {
-            connectionFactory.setPassword(password);
-        }
-
-        // 网络异常自动连接恢复
-        connectionFactory.setAutomaticRecoveryEnabled(true);
-        // 每5秒尝试重试连接一次
-        connectionFactory.setNetworkRecoveryInterval(5000L);
-
-        //创建连接
-        connection = connectionFactory.newConnection();
-
-        channel = connection.createChannel();
-        // 启用消息确认
-        channel.confirmSelect();
-        eventPropsDefault = newEventProps().build();
-
     }
 
     public boolean basicPublish(String exchangeName, String topic, String event_data) throws Exception {
