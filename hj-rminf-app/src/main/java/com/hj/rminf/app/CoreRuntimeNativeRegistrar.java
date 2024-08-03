fba4f90db045c3b8746fbe0e7f2d68cc6cff0b46
wuyilong <791128058@qq.com>
2024-08-03 08:56:25 +0800

    打包

---
+++ hj-rminf-app/src/main/java/com/hj/rminf/app/CoreRuntimeNativeRegistrar.java
@@ -0,0 +1,24 @@
+package com.hj.rminf.app;
+
+import org.noear.solon.annotation.Component;
+import org.noear.solon.aot.RuntimeNativeMetadata;
+import org.noear.solon.aot.RuntimeNativeRegistrar;
+import org.noear.solon.core.AppContext;
+
+/**
+ * @author wyl
+ * @date 2024年08月03日 8:45
+ */
+@Component
+public class CoreRuntimeNativeRegistrar implements RuntimeNativeRegistrar {
+
+    @Override
+    public void register(AppContext context, RuntimeNativeMetadata metadata) {
+        metadata.registerArg("-J--add-opens=java.base/java.lang.invoke=ALL-UNNAMED");
+        metadata.registerArg("-J--add-opens=java.base/java.nio=ALL-UNNAMED");
+        metadata.registerArg("-J--add-exports=java.base/jdk.internal.misc=ALL-UNNAMED");
+
+        metadata.registerArg("-march=compatibility");
+    }
+
+}
