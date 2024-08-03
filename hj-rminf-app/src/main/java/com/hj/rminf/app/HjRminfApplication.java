25521fa50373bdb62bbaf8e51c9b9dd7977d6530
wuyilong <791128058@qq.com>
2024-08-03 16:48:47 +0800

    打包

--- hj-rminf-app/src/main/java/com/hj/rminf/app/HjRminfApplication.java
+++ hj-rminf-app/src/main/java/com/hj/rminf/app/HjRminfApplication.java
@@ -37,9 +37,9 @@ public class HjRminfApplication {
                 }
             });*/
         });
-        if (!Solon.cfg().getBool("solon.aot", false)) {
+        /*if (!Solon.cfg().getBool("solon.aot", false)) {
             Endpoint.publish(Solon.cfg().get("webservice.addr"), new TransWebServiceImpl());
-        }
+        }*/
         // WebServiceHelper.createWebClient(Solon.cfg().get("webservice.addr"), TransWebService.class);
         // EndpointImpl endpoint = new EndpointImpl(BusFactory.getDefaultBus(), new TransWebServiceImpl());
         // endpoint.publish("/service");
