c77cd5fe45c99f3de96659a9dfba26ff2374cae2
79112 <791128058@qq.com>
2024-11-01 17:16:19 +0800

    打包

--- hj-rminf-app/src/main/java/com/hj/rminf/app/HjRminfApplication.java
+++ hj-rminf-app/src/main/java/com/hj/rminf/app/HjRminfApplication.java
@@ -26,7 +26,7 @@ public class HjRminfApplication {
     }
 
     public static void main(String... args) {
-        // 1111
+        // 2222
         globalArgs = args;
         Quarkus.run(args);
     }
