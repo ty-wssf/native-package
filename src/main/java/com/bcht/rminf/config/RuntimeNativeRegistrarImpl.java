cffd4c428212ec6f6896e6bfc1d5e5c2ce6c5777
79112 <791128058@qq.com>
2024-11-20 11:10:57 +0800

    打包

--- src/main/java/com/bcht/rminf/config/RuntimeNativeRegistrarImpl.java
+++ src/main/java/com/bcht/rminf/config/RuntimeNativeRegistrarImpl.java
@@ -4,6 +4,7 @@ import com.github.xiaoymin.knife4j.solon.settings.OpenApiSetting;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.aot.RuntimeNativeMetadata;
 import org.noear.solon.aot.RuntimeNativeRegistrar;
+import org.noear.solon.aot.hint.MemberCategory;
 import org.noear.solon.core.AppContext;
 
 @Component
@@ -11,7 +12,7 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
 
     @Override
     public void register(AppContext context, RuntimeNativeMetadata metadata) {
-        metadata.registerReflection(OpenApiSetting.class);
+        metadata.registerReflection(OpenApiSetting.class, MemberCategory.values());
     }
 
 }
