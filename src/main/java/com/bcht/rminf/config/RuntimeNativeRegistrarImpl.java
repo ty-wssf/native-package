e16bbdf4603929bd4fa5dc5b8db10fc4b525a4c6
79112 <791128058@qq.com>
2024-11-25 14:44:33 +0800

    打包

--- src/main/java/com/bcht/rminf/config/RuntimeNativeRegistrarImpl.java
+++ src/main/java/com/bcht/rminf/config/RuntimeNativeRegistrarImpl.java
@@ -1,18 +1,6 @@
 package com.bcht.rminf.config;
 
-import com.bcht.rminf.modules.demo.model.Author;
-import com.bcht.rminf.modules.demo.model.Book;
-import com.bcht.rminf.modules.demo.model.BookStore;
-import com.bcht.rminf.modules.demo.model.TreeNode;
 import com.bcht.rminf.modules.demo.model.common.BaseEntity;
-import com.bcht.rminf.modules.hkpostgresql.model.WarningInstanceInfo;
-import com.bcht.rminf.modules.hkpostgresql.model.WarningInstanceInfoDraft;
-import com.bcht.rminf.modules.hkpostgresql.model.WeatherInfo;
-import com.bcht.rminf.modules.hkpostgresql.model.WeatherInfoDraft;
-import com.bcht.rminf.modules.sys.model.SysConfig;
-import com.bcht.rminf.modules.sys.model.SysConfigDraft;
-import com.bcht.rminf.modules.sys.model.SysRelation;
-import com.bcht.rminf.modules.sys.model.SysRelationDraft;
 import com.github.xiaoymin.knife4j.solon.settings.OpenApiExtendSetting;
 import com.github.xiaoymin.knife4j.solon.settings.OpenApiSetting;
 import org.noear.solon.annotation.Component;
@@ -20,6 +8,8 @@ import org.noear.solon.aot.RuntimeNativeMetadata;
 import org.noear.solon.aot.RuntimeNativeRegistrar;
 import org.noear.solon.aot.hint.MemberCategory;
 import org.noear.solon.core.AppContext;
+import org.noear.solon.core.util.ClassUtil;
+import org.noear.solon.core.util.ScanUtil;
 
 @Component
 public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
@@ -29,22 +19,14 @@ public class RuntimeNativeRegistrarImpl implements RuntimeNativeRegistrar {
         metadata.registerReflection(OpenApiSetting.class, MemberCategory.values());
         metadata.registerReflection(OpenApiExtendSetting.class, MemberCategory.values());
         metadata.registerReflection(BaseEntity.class, MemberCategory.values());
-        metadata.registerReflection(Author.class, MemberCategory.values());
-        metadata.registerReflection(Book.class, MemberCategory.values());
-        metadata.registerReflection(BookStore.class, MemberCategory.values());
-        metadata.registerReflection(TreeNode.class, MemberCategory.values());
-        metadata.registerReflection(WeatherInfo.class, MemberCategory.values());
-        metadata.registerReflection(WarningInstanceInfo.class, MemberCategory.values());
-        metadata.registerReflection(SysConfig.class, MemberCategory.values());
-        metadata.registerReflection(SysRelation.class, MemberCategory.values());
-        metadata.registerReflection(AuthorDraft.class, MemberCategory.values());
-        metadata.registerReflection(BookDraft.class, MemberCategory.values());
-        metadata.registerReflection(BookStoreDraft.class, MemberCategory.values());
-        metadata.registerReflection(TreeNodeDraft.class, MemberCategory.values());
-        metadata.registerReflection(WeatherInfoDraft.class, MemberCategory.values());
-        metadata.registerReflection(WarningInstanceInfoDraft.class, MemberCategory.values());
-        metadata.registerReflection(SysConfigDraft.class, MemberCategory.values());
-        metadata.registerReflection(SysRelationDraft.class, MemberCategory.values());
+        //扫描类文件并处理（采用两段式加载，可以部分bean先处理；剩下的为第二段处理）
+        ScanUtil.scan(context.getClassLoader(), "com/bcht", n -> n.endsWith(".class"))
+                .forEach(name -> {
+                    String className = name.substring(0, name.length() - 6);
+                    className = className.replace('/', '.');
+                    Class<?> clz = ClassUtil.loadClass(context.getClassLoader(), className);
+                    metadata.registerReflection(clz, MemberCategory.values());
+                });
     }
 
 }
